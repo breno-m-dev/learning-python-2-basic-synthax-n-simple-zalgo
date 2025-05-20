b06ca185d2a775450b7d99e12206c0484f108c19
unknown <brenomdev@protonmail.com>
2025-05-20 10:59:55 -0300

    The diacritic adding doesn't work, they are not being added to the characters. The problem is I was using separate diacritics like this ' ^ ~. These are considered characters themselves, not diacritics. I have to use their unicode code, to be abble to add the diacritics to the characters

--- python-2-basic-synthax-n-simple-zalgo.py
+++ python-2-basic-synthax-n-simple-zalgo.py
@@ -1,7 +1,14 @@
 #this code is meant to be just some synthax tests to learn the basics
 #I will try to make a simple Zalgo(glitchy text), by concateneting some diacritics into a string
 
+the_str = 'a'
 zalgo = True
 if(zalgo==True):
     print('zalgo text will appear soon!')
 
+diacritics = ["^", "~", "'"]
+
+for diacritic in diacritics:
+    the_str = the_str+diacritic
+
+print(the_str)
