f7434eaa71a796074ac2a91017355976e8b630d5
unknown <brenomdev@protonmail.com>
2025-05-20 10:50:22 -0300

    Start of the program. It still doesn't do much, I just tested if statement and a boolean variable

---
+++ python-2-basic-synthax-n-simple-zalgo.py
@@ -0,0 +1,7 @@
+#this code is meant to be just some synthax tests to learn the basics
+#I will try to make a simple Zalgo(glitchy text), by concateneting some diacritics into a string
+
+zalgo = True
+if(zalgo==True):
+    print('zalgo text will appear soon!')
+
