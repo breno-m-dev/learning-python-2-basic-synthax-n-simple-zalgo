98d15de88a9ac443e39e2cdee2a0b4ff16f19596
unknown <brenomdev@protonmail.com>
2025-05-22 20:22:00 -0300

    The script now can turn a string with multiple characters into zalgo text. The zalgo distortion is still very simple though

--- python-2-basic-synthax-n-simple-zalgo.py
+++ python-2-basic-synthax-n-simple-zalgo.py
@@ -6,13 +6,18 @@ zalgo = True
 if(zalgo==True):
     print('zalgo text will appear soon!')
 
-
 #\u0300 = grave `
 #\u302 = circumflex ^
 #\u303 = tilde ~
-diacritics = ["\u0300","\u0302", "\u0303" ]
+## usar isso da no mesmo que a linha de baixo!! diacritics = ["\u0300","\u0302", "\u0303","\u0300","\u0302", "\u0303","\u0300","\u0302", "\u0303","\u0300","\u0302", "\u0303" ]
+diacritics = ["\u0300","\u0302", "\u0303"]
+input ="oioi tudo bem?"
+outputStr =""
 
-for diacritic in diacritics:
-    the_str = the_str+diacritic
+for char in input:
+    outputStr += char #adiciona 1 char a string de saida
+    for diacritic in diacritics: #adiciona os diacriticos no ultima char adicionado a string de saida
+        outputStr += diacritic 
 
 print(the_str)
+print("Output:"+outputStr)
